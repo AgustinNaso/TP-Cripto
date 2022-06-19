c977fbdce0f16f73ac25351b3775f1a9e6fb104a
Agustin Naso <anaso@itba.edu.ar>
2022-06-19 17:47:53 -0300

    fix: lsb1 and lsb4

--- include/utils.h
+++ include/utils.h
@@ -5,6 +5,16 @@
 // #include "types.h"
 #define GET_NTH_LSB(byte, n) ((byte >> (n-1) & 1))
 #define GET_INT_FROM_2_BITS(bit1, bit2) (2*bit1 + bit2)
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
 
 const char* getFileExtension(const char * path);
 int modifyBit(unsigned char byte, int position, int bit);
